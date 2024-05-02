ba55e902448be493086e6b45f128db9869953a9c
Kd-13-super <2237023838@qq.com>
2024-05-03 00:01:25 +0800

    Site updated: 2024-05-03 00:01:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/29/hello-world/","2024/05/02/hexo初步搭建完成/","2024/05/02/test/"];function toRandomPost(){
+var posts=["2024/04/29/hello-world/","2024/05/02/hexo初步搭建完成/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
