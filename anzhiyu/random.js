ddb00a1164e5324315b13f59363f4ace27ee98bc
Kd-13-super <2237023838@qq.com>
2024-05-30 00:26:35 +0800

    Site updated: 2024-05-30 00:26:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/03/Mybatis注解查询/","2024/05/26/SpringMVC交互性网页/","2024/05/07/Vue-Element前端页面快速构建/","2024/05/17/Vue-Element通用管理界面/","2024/04/29/hello-world/","2024/05/02/hexo初步搭建完成/","2024/05/25/maven-springmvc创建网页/","2024/05/16/spring后端交互Vue前端/","2024/05/26/stm32点亮小灯泡/","2024/05/06/控制反转和依赖注入/","2024/05/23/初识STM32/","2024/05/12/面向切面编程AOP/","2024/05/29/SpringMVC数据绑定/"];function toRandomPost(){
+var posts=["2024/05/03/Mybatis注解查询/","2024/05/26/SpringMVC交互性网页/","2024/05/07/Vue-Element前端页面快速构建/","2024/05/17/Vue-Element通用管理界面/","2024/04/29/hello-world/","2024/05/02/hexo初步搭建完成/","2024/05/25/maven-springmvc创建网页/","2024/05/16/spring后端交互Vue前端/","2024/05/26/stm32点亮小灯泡/","2024/05/06/控制反转和依赖注入/","2024/05/23/初识STM32/","2024/05/12/面向切面编程AOP/","2024/05/29/SpringMVC数据绑定/","2024/05/29/Eclipse创建SpringMVC项目/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
